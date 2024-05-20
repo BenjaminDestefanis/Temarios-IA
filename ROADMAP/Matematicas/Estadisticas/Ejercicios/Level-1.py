e65adb137524cf77a57776deb3a6a64ee527c175
BenjaminDestefanis <benjaminisraelb@gmail.com>
2024-05-20 10:14:38 -0300

    5/20/24 10:14 PM

--- ROADMAP/Matematicas/Estadisticas/Ejercicios/Level-1.py
+++ ROADMAP/Matematicas/Estadisticas/Ejercicios/Level-1.py
@@ -1,7 +1,16 @@
 # 1. Calcular la media, la mediana y la moda
 
 def calular_estadisticas(datos):
-    from collections import Counter
-    from statistics import mean, median, mode, multimode
+    # La funcion recibe una lista de numeros y debe retornar
+    # un dicctionario con los 3 valores de la siguiente manera:
+    # {'media': valor, 'mediana': valor, 'moda': valor}
+    # TU CODIGO AQUI:
+    return datos
+
+
+# 2. Calculo de varianza
+
+def calcular_varianza(datos):
+    return datos
 
     
