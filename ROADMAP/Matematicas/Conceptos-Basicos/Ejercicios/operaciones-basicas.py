238ba0719a8462161bb3b3f205519addd10cde42
BenjaminDestefanis <benjaminisraelb@gmail.com>
2024-05-23 14:46:44 -0300

    5/23/24 2:46 PM

--- ROADMAP/Matematicas/Conceptos-Basicos/Ejercicios/operaciones-basicas.py
+++ ROADMAP/Matematicas/Conceptos-Basicos/Ejercicios/operaciones-basicas.py
@@ -11,5 +11,85 @@ EJ: La raiz cuadrada de 2 , PI etc
 
 Numeros Reales (R): Incluye los numeros Racionales e Irracionale. Estos pueden ser cualquier numero que se puede ubicar en una recta
 
+Punto a tener en cuenta de la suma/resta/division/multiplicacion:
+
+Suma (+):
+
+Propiedades:
+
+Conmutativa:  a + b = b + a .
+Asociativa: (a + b ) + c = a + ( b + c).
+Elemento Neutro: a + 0 = a .
+
+Resta (-):
+
+Encuentra la diferencia entre dos números.
+No es conmutativa:  a - b ≠ b - a .
+
+No es asociativa: (a -b) - c ≠ a - (b - c) .
+
+Multiplicación (*):
+
+Combina dos números para obtener su producto.
+
+Propiedades:
+
+Conmutativa: a * b = b * a .
+
+Asociativa: (a * b ) * c = a * ( b * c ).
+
+Elemento Neutro: a * 1 = 0
+
+Distributiva sobre la suma: a * ( b + c ) = (a * b) + (a * c) 
+
+División (÷):
+
+Encuentra cuántas veces un número está contenido en otro.
+No es conmutativa: a / b ≠ b / c
+No es asociativa: (a / b) / c ≠ a / (b / c)
+
+La división por cero no está definida.
+
+Propiedades de las Operaciones
+Propiedad Conmutativa:
+
+Para la suma y la multiplicación: cambiar el orden de los números no cambia el resultado.
+
+3+5 = 5+3
+4x7 = 7x4
+
+Para la suma y la multiplicación: la forma en que se agrupan los números no cambia el resultado.
+
+Propiedad Distributiva:
+
+Multiplicar un número por una suma es lo mismo que multiplicar el número por cada sumando y luego sumar los productos.
+
+Potencias y Raíces
+
+Potencias:
+
+Expresan la multiplicación repetida de un número por sí mismo.
+
+a a la n - significa a * a , tantas veces como dice n.
+
+
+Propiedades:
+- (n) en esta ocacion significa "elevado a la n"
+a(n) x a(m) = a(n + m).
+
+(a(n))(m) = a(n x m).
+
+a(m) / a(n) = a(m-n)
+
+Raíces:
+
+La raíz cuadrada de un número 'a' es un numero b(2) = a
+
+Propiedades :
+√ = Equivale a raiz Cuadrada
+√a x b
+
+
+
 
 """
