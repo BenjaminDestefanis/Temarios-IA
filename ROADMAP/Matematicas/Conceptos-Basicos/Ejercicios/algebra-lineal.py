bcc8b92338c03b3a443b5f1840039b49c173093c
BenjaminDestefanis <benjaminisraelb@gmail.com>
2024-05-24 10:34:25 -0300

    5/24/24 10:34 AM

--- ROADMAP/Matematicas/Conceptos-Basicos/Ejercicios/algebra-lineal.py
+++ ROADMAP/Matematicas/Conceptos-Basicos/Ejercicios/algebra-lineal.py
@@ -30,7 +30,40 @@ Un espacio vectorial es un copnjunto de vectores que se pueden sumar entre si, p
 
 """
 
+# EJERCICIOS
+
 #1. Calculo de magnitud y direccion de un vector : 
 # Tenemos un vector (v = [2, -3, 4]). Debemos calcular la magnitud (modulo) y la direccion del vector.
 # Sea otro vector (u = [-1, 5, 2] ). Debemos calcular el angulo entre u y el eje positivo.
-#
+
+
+#2. Verificacion de subespacio
+# Recibiremos un conjunto de vectores en R^3, la funcion debe ferificar si estos forman un subespacio
+# TIPS: Un conjunto de vectorers jforma un sibespacio si cumple con 3 condiciones:
+# A. El Vector 0 esta en el conjunto. B.Esta cerrado bajo la suma de vectores. C. Esta cerrado bajo la multiplicacion por escalares
+
+# SOLUCION [Recordad que las soluciones deben estar en otro documento]
+
+# C
+
+import numpy as np
+
+def is_subspace(vectors):
+    # Verificar si el vector cero está en el conjunto
+    zero_vector = np.zeros_like(vectors[0])
+    if not any((vector == zero_vector).all() for vector in vectors):
+        return False
+
+    # Verificar la cerradura bajo la suma de vectores
+    for v1 in vectors:
+        for v2 in vectors:
+            if not any((v1 + v2 == v).all() for v in vectors):
+                return False
+
+    # Verificar la cerradura bajo la multiplicación por escalares
+    for v in vectors:
+        for scalar in range(-10, 10):
+            if not any((scalar * v == v2).all() for v2 in vectors):
+                return False
+
+    return True
