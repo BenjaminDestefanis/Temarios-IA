0988770034670507b9f1080a61c194d96d370b96
BenjaminDestefanis <benjaminisraelb@gmail.com>
2024-05-25 23:20:44 -0300

    25/05/24 23:20 Hs

---
+++ ROADMAP/SQL/info.sql
@@ -0,0 +1,9 @@
+-- Acceso a Bases de Datos
+-- psql --username=<nombre de usuario> --dbname=<nombre DB>
+
+-- Muestra de lista de informacion
+-- \l
+
+-- Crear Base de datos
+CREATE DATABASE --<database_name>;
+
