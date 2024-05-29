be5ae3020609e7ab3524c196ca210b10287e4b5d
BenjaminDestefanis <benjaminisraelb@gmail.com>
2024-05-29 18:05:21 -0300

    5/29/24 6:05PM

---
+++ ROADMAP/SQL/Steaments/INSERT.sql
@@ -0,0 +1,7 @@
+-- EJEMPLOS
+
+-- 1.
+INSERT INTO table_name (column1, column2, column3, ...)
+VALUES (value1, value2, value3, ...);
+
+-- Aqui se especifica tanto los nombres de las columnas como los valores que se insertarán.
