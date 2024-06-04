894e6d55382ded34f76e8a7e9aa978761805db35
BenjaminDestefanis <benjaminisraelb@gmail.com>
2024-06-04 17:19:49 -0300

    6/4/24 5:19PM

---
+++ ROADMAP/Python/1-Numpy/class1.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+# Estos array tambien se los conoce como vectores.
+# Creacion de Array unifimensional.
+
+vector_unidimensional = np.array([1,2,3,4,5])
+
+# Array bidimensional
+
+vector_bidimensional = np.array([[1,2,3], [4,5,6]])
+
+# Array tridimensional
+
+vector_tridimensional = np.array([[[1,2,3], [4,5,6]],[[7,8,9], [10,11,12]]])
+
+print(vector_unidimensional, vector_bidimensional, vector_tridimensional)
+
+
