3264ca41ebe69d7adfe1be2b3fdea4422f9b8245
BenjaminDestefanis <benjaminisraelb@gmail.com>
2024-06-05 10:35:24 -0300

    6/5/24 10:35 AM

---
+++ ROADMAP/Python/Computing-Scientific-With-Python/basic.py
@@ -0,0 +1,18 @@
+# Variables
+
+#Variables are an essential part of Python and any programming language. A variable is a name that references or points to an object. You can declare a variable by writing the variable name on the left side of the assignment operator = and specifying the value to assign to that variable on the right side of the assignment operator:
+
+variable_name = 'value'
+
+#Create a variable called number and assign the value 5 to your new variable.
+
+
+#Variables can store values of different data types. You just assigned an integer value, but if you want to represent some text, you need to assign a string. Strings are sequences of characters enclosed by single or double quotes, but you cannot start a string with a single quote and end it with a double quote or vice versa:
+
+string_1 = "I am a string"
+string_2 = 'I am also a string'
+# string_3 = 'This is not valid"
+
+#Delete your number variable and its value. Then, declare another variable called text and assign the string 'Hello World' to this variable.
+
+
