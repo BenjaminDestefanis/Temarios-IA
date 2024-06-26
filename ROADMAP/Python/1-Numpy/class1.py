978103f8c729a477984cecd4225850fa5d8ad2ab
BenjaminDestefanis <benjaminisraelb@gmail.com>
2024-06-26 11:15:32 -0300

    6/26/24 11:15 AM

--- ROADMAP/Python/1-Numpy/class1.py
+++ ROADMAP/Python/1-Numpy/class1.py
@@ -1,5 +1,10 @@
 import numpy as np
+import colorama
+colorama.init()
 
+fore  = colorama.Fore
+reset_style = colorama.Style.RESET_ALL
+ 
 # Estos array tambien se los conoce como vectores.
 # Creacion de Array unifimensional.
 
@@ -13,9 +18,7 @@ vector_bidimensional = np.array([[1,2,3], [4,5,6]])
 
 vector_tridimensional = np.array([[[1,2,3], [4,5,6]],[[7,8,9], [10,11,12]]])
 
-print('Vector unidiemsional: ', vector_unidimensional,
-      'Vector bidimensional:', vector_bidimensional,
-      'Vector tridimensional', vector_tridimensional)
+print( f"Vector unidiemsional: {vector_unidimensional} \n Vector bidimensional: {vector_bidimensional} \n Vector tridimensional: {vector_tridimensional} ")
 
 
 # Funciones o metodos array en NUMPY
@@ -23,24 +26,24 @@ print('Vector unidiemsional: ', vector_unidimensional,
 # Array de ceros - zeros(cant. de arrays, cant. de valores)
 zeros = np.zeros((2, 3))
 
-print("'ZEROS:'", zeros)
+print(f"ZEROS: {zeros} \n")
 
 # Array de unos - ones(cant. de arrays, cant. de valores)
 ones = np.ones((3, 3))
 
-print("'ONES:'", ones)
+print(f"ONES: {ones} \n")
 
 # Array con valores aleatorios - rand(cant. de arrays, cant. de valores)
 random = np.random.rand(2, 2)
 
-print("'RAND:'", random)
+print(f"{fore.MAGENTA}RANDOM: {reset_style}{random} \n " )
 
 # Array con un rango de valores - range_array(numero inicial, numero final, intervalo)
 range_array = np.arange(0, 10, 3)
 
-print("'RANGE ARRAY:'", range_array)
+print(f"RANGE ARRAY: {range_array} \n")
 
 # Array con valores equidistantes - profuncdizar
 linspace_array = np.linspace(0, 1, 5)
 
-print("'LINSPACE ARRAY:'", linspace_array)
+print(f"LINSPACE ARRAY: {linspace_array} \n")
--- ROADMAP/Python/Projects/Projec-1/index.py
+++ ROADMAP/Python/Projects/Projec-1/index.py
@@ -2,8 +2,8 @@
 
 import requests
 
-api_key = 'e278246514e613608d97c59f'
-url = f'https://v6.exchangerate-api.com/v6/{api_key}/latest/USD'
+API_KEY = 'e278246514e613608d97c59f'
+BASE_URL = f'https://v6.exchangerate-api.com/v6/{API_KEY}/latest/USD'
 print(url)
 response = requests.get(url)
 data = response.json()
